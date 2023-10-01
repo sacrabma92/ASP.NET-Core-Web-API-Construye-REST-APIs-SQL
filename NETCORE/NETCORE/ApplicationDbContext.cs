21858d10048e234a222eab95489ae8c3f256e68d
Carlos Ramirez <karlossander92@gmail.com>
2023-10-01 18:20:01 -0500

    DbContext

--- NETCORE/NETCORE/ApplicationDbContext.cs
+++ NETCORE/NETCORE/ApplicationDbContext.cs
@@ -11,8 +11,17 @@ namespace NETCORE
             optionsBuilder.UseSqlServer(connectionString);
         }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CursoInstructor>().HasKey(ci => new { ci.CursoId, ci.InstructorId });
+
+            base.OnModelCreating(modelBuilder);
+        }
+
         public DbSet<Curso> Curso { get; set; }
         public DbSet<Precio> Precio { get; set; }
         public DbSet<Comentario> Comentario { get; set; }
+        public DbSet<CursoInstructor> CursoInstructor { get; set; }
+        public DbSet<Instructor> Instructor { get; set; }
     }
 }
